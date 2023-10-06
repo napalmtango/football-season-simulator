88cd085ff31306e4f820b3ba7e17d3e598677c68
NapalmTango <142678424+NapalmTango@users.noreply.github.com>
2023-10-05 19:14:16 -0700

    standings html structure, initialized standings array

---
+++ league.js
@@ -0,0 +1,7 @@
+const league = [{team: "Rams", record: [3, 1, 0, 100, 70]},
+                {team: "49ers", record: [2, 1, 1, 90, 80]},
+                {team: "Seahawks", record: [1, 2, 1, 80, 90]},
+                {team: "Cardinals", record: [1, 3, 0, 70, 100]}];
+
+console.table(league);
+
